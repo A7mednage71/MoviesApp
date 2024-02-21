89da0deba9cd0993b325fe03c477c36d10ea84a5
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-02-22 00:07:36 +0200

    add Either for HomeMoviesRepo

--- lib/Features/HomeScreen/domain/Repos/HomeRepo.dart
+++ lib/Features/HomeScreen/domain/Repos/HomeRepo.dart
@@ -1,7 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies/core/Errors/Failure.dart';
+
 import '../Entites/MovieEntity.dart';
 
 abstract class HomeMoviesRepo {
-  Future<List<MovieEntity>> fetchNowPlaying();
-  Future<List<MovieEntity>> fetchPopularMovies();
-  Future<List<MovieEntity>> fetchTopRatedMovies();
+  Future<Either<Failure,List<MovieEntity>>> fetchNowPlaying();
+  Future<Either<Failure,List<MovieEntity>>> fetchPopularMovies();
+  Future<Either<Failure,List<MovieEntity>>> fetchTopRatedMovies();
 }
